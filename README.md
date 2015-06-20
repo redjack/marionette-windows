2284a1dc37bc9a5b5947f329b896308d82282aab
Kevin P. Dyer <kpdyer@gmail.com>
2015-06-20 10:16:54 -0700

    ...

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ```console
 git clone git@github.com:kpdyer/marionette-windows-builder.git
-cd marionette-windows-builder
-cd build
+cd marionette-windows-builder/build
 git clone git@github.com:redjack/marionette.git
+cd ..
 vagrant up
 ... wait ...
 ls -al dist
--- build_marionette.py
+++ build_marionette.py
@@ -10,24 +10,23 @@ def main():
         print 'Are you sure your envs are set? Run \'source setenv.sh\' in /vagrant.'
         sys.exit(1)
 
-
     tasks_to_do = [
-        # marionette_windows.tasks.MakeBinDirTask,
-        # marionette_windows.tasks.MakeLibDirTask,
-        # marionette_windows.tasks.InstallPrereqsTask,
-        # marionette_windows.tasks.InitWineTask,
-        # marionette_windows.tasks.InstallPythonTask,
-        # marionette_windows.tasks.InstallSetuptoolsTask,
-        # marionette_windows.tasks.InstallPy2EXETask,
-        # marionette_windows.tasks.InstallWineWrappers,
-        # marionette_windows.tasks.InstallDlfcnTask,
-        # marionette_windows.tasks.InstallMmanTask,
-        # marionette_windows.tasks.InstallRegex2DFATask_openfst,
-        # marionette_windows.tasks.InstallRegex2DFATask_re2,
-        # marionette_windows.tasks.InstallRegex2DFATask,
-        # marionette_windows.tasks.InstallGMPTask,
-        # marionette_windows.tasks.InstallPyCryptoTask,
-        # marionette_windows.tasks.InstallFTETask,
+        marionette_windows.tasks.MakeBinDirTask,
+        marionette_windows.tasks.MakeLibDirTask,
+        marionette_windows.tasks.InstallPrereqsTask,
+        marionette_windows.tasks.InitWineTask,
+        marionette_windows.tasks.InstallPythonTask,
+        marionette_windows.tasks.InstallSetuptoolsTask,
+        marionette_windows.tasks.InstallPy2EXETask,
+        marionette_windows.tasks.InstallWineWrappers,
+        marionette_windows.tasks.InstallDlfcnTask,
+        marionette_windows.tasks.InstallMmanTask,
+        marionette_windows.tasks.InstallRegex2DFATask_openfst,
+        marionette_windows.tasks.InstallRegex2DFATask_re2,
+        marionette_windows.tasks.InstallRegex2DFATask,
+        marionette_windows.tasks.InstallGMPTask,
+        marionette_windows.tasks.InstallPyCryptoTask,
+        marionette_windows.tasks.InstallFTETask,
         marionette_windows.tasks.InstallMarionetteTask,
     ]
     for task in tasks_to_do:
